f9c7d54aabf4acccab2379d85395bd8e3351082d
Fromnowon <zihangzhao@gmail.com>
2018-09-05 17:29:52 +0800

    update

---
+++ js/new.js
@@ -0,0 +1,55 @@
+$(function () {
+    var container = $('.container');
+
+    //增加备选答案
+    container.on('click', '.add_answer', function () {
+        //限制答案数量
+        if ($(this).prev().attr('num') == '10') {
+            alert('备选答案应不多于10个');
+        }
+        else {
+            $(this).prev().after($('.choose_default').clone()).next().removeClass('choose_default').removeClass('hide');
+            $(this).prev().attr('num', parseInt($(this).prev().prev().attr('num')) + 1);
+        }
+    })
+    //删除备选答案
+    container.on('click', '.del_answer', function () {
+        //重新处理num值
+        var next = $(this).parent().next();//获取即将被删除div的下一个元素
+        while (next.prop('tagName') == 'DIV') {
+            next.attr('num', parseInt(next.attr('num')) - 1);
+            next = next.next();
+        }
+        $(this).parent().remove();
+    })
+
+    //指定标准答案
+    container.on('click', '.set_right_answer', function () {
+        if ($(this).parent().hasClass('right_answer')) {
+            $(this).parent().removeClass('right_answer').find('.fa-check').css('color', '#cbcbcb');
+        } else {
+            //互斥，清除其他标准答案
+            $('.right_answer').removeClass('right_answer').find('.fa-check').css('color', '#cbcbcb');
+            $(this).parent().addClass('right_answer').find('.fa-check').css('color', 'black');
+        }
+    })
+
+    //新增题目
+    container.on('click', '.add_exercise', function () {
+        $(this).parent().before($('.exercise_default').clone()).prev().removeClass('exercise_default').css('display', 'block');
+        $(this).parent().prev().find('.exercise_num').text(parseInt($(this).parent().prevAll('div').eq(1).find('.exercise_num').text()) + 1);
+        $('.exercise_total').text(parseInt($('.exercise_total').text()) + 1);
+    })
+
+    //删除题目
+    container.on('click', '.del_exercise', function () {
+        //重新处理题目序号
+        var next = $(this).parents('div').eq(0).next();//获取下一题div
+        while (next.prop('tagName') == 'DIV') {
+            next.find('.exercise_num').text(parseInt(next.find('.exercise_num').text()) - 1);
+            next = next.next();
+        }
+        $(this).parents('div').eq(0).remove();
+        $('.exercise_total').text(parseInt($('.exercise_total').text()) - 1);
+    })
+})
--- main/new.php
+++ main/new.php
@@ -14,6 +14,7 @@ if ($_SESSION['validate'] != 1) {
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>生成题组</title>
     <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/font-awesome.min.css">
     <link rel="stylesheet" href="../css/new.css">
     <script src="../js/jquery.min.js"></script>
     <script src="../js/bootstrap.js"></script>
@@ -21,26 +22,109 @@ if ($_SESSION['validate'] != 1) {
 </head>
 <body>
 <div class="container">
+    <div style="margin-top: 20px">
+        <p style="font-weight: bold">题目数量：<span class="exercise_total">1</span></p>
+    </div>
     <div class="exercise">
-        <table>
-            <tr>
-                <td style="width: 30px;text-align: center" valign="top" rowspan="2"><span class="exercise_num" >1</span>.</td>
-                <td class="exercise_content">这是一道样例这是一道样例这是一道样例这是一道样例这是一道样例这是一道样例这是一道样例这是一道样例这是一道样例这是一道样例</td>
-            </tr>
-            <tr>
-                <td class="exercise_choose" style="padding-top: 10px">
+        <div class="exercise_default exercise_div" style="display:none;margin-bottom:20px;padding: 10px">
+            <table>
+                <tr>
+                    <td colspan="2" height="30px"><a href="javascript:void(0)" class="pull-right del_exercise"><i
+                                    style="transform: scale(1.1);" class="icon-remove"></i></a></td>
+                </tr>
+                <tr>
+                    <td style="width: 30px;text-align: center" valign="top" rowspan="2"><span
+                                class="exercise_num">0</span>.
+                    </td>
+                    <td class="exercise_content"><textarea style="margin-left: 20px;width: 90%;height: 80px"></textarea>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="exercise_choose" style="padding-top: 10px">
+                        <div class="exercise_answer choose_default hide" style="position: relative;">
+                            <input class="answer" style="margin-left: 20px;width: 400px;" type="text"
+                                   placeholder="备选答案">
+                            <a href="javascript:void(0)" class="del_answer"><i
+                                        style="position: absolute;left: 480px;top:8px;color: #d50000"
+                                        class="fa fa-minus"></i></a>
+                            <a href="javascript:void(0)" title="设为标准答案" class="set_right_answer"><i
+                                        style="color:#cbcbcb;position: absolute;left: 450px;top:6px"
+                                        class="fa fa-check"></i></a>
+                        </div>
+                        <div class="exercise_answer" style="position: relative;" num="0">
+                            <input class="answer" style="margin-left: 20px;width: 400px" type="text" placeholder="备选答案">
+                            <a href="javascript:void(0)" title="设为标准答案" class="set_right_answer"><i
+                                        style="color:#cbcbcb;position: absolute;left: 450px;top:6px"
+                                        class="fa fa-check"></i></a>
+                        </div>
+                        <div class="exercise_answer" style="position: relative;" num="1">
+                            <input class="answer" style="margin-left: 20px;width: 400px" type="text" placeholder="备选答案">
+                            <a href="javascript:void(0)" title="设为标准答案" class="set_right_answer"><i
+                                        style="color:#cbcbcb;position: absolute;left: 450px;top:6px"
+                                        class="fa fa-check"></i></a>
+                        </div>
+                        <a href="javascript:void(0)" class="add_answer"><i
+                                    style="transform: scale(1.2);margin-left: 20px;" class="icon-plus"></i></a>
+                    </td>
+                </tr>
+            </table>
+            <div style="margin-top:20px;height:10px;border-bottom:1px solid darkgray"></div>
+        </div>
+        <div class="exercise_div" style="margin-bottom:20px;padding: 10px">
+            <table>
+                <tr>
+                    <td colspan="2" height="30px"><a href="javascript:void(0)" class="pull-right del_exercise"><i
+                                    style="transform: scale(1.1);" class="icon-remove"></i></a></td>
+                </tr>
+                <tr>
+                    <td style="width: 30px;text-align: center" valign="top" rowspan="2"><span
+                                class="exercise_num">1</span>.
+                    </td>
+                    <td class="exercise_content">
+                        <textarea style="margin-left: 20px;width: 90%;height: 80px"></textarea>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="exercise_choose" style="padding-top: 10px">
+                        <div class="exercise_answer choose_default hide" style="position: relative;">
+                            <input class="answer" style="margin-left: 20px;width: 400px;" type="text"
+                                   placeholder="备选答案">
+                            <a href="javascript:void(0)" class="del_answer"><i
+                                        style="position: absolute;left: 480px;top:8px;color: #d50000"
+                                        class="fa fa-minus"></i></a>
+                            <a href="javascript:void(0)" title="设为标准答案" class="set_right_answer"><i
+                                        style="color:#cbcbcb;position: absolute;left: 450px;top:6px"
+                                        class="fa fa-check"></i></a>
+                        </div>
+                        <div class="exercise_answer" style="position: relative;" num="0">
+                            <input class="answer" style="margin-left: 20px;width: 400px" type="text" placeholder="备选答案">
+                            <a href="javascript:void(0)" title="设为标准答案" class="set_right_answer"><i
+                                        style="color:#cbcbcb;position: absolute;left: 450px;top:6px"
+                                        class="fa fa-check"></i></a>
+                        </div>
+                        <div class="exercise_answer" style="position: relative;" num="1">
+                            <input class="answer" style="margin-left: 20px;width: 400px" type="text" placeholder="备选答案">
+                            <a href="javascript:void(0)" title="设为标准答案" class="set_right_answer"><i
+                                        style="color:#cbcbcb;position: absolute;left: 450px;top:6px"
+                                        class="fa fa-check"></i></a>
+                        </div>
+                        <a href="javascript:void(0)" class="add_answer"><i
+                                    style="transform: scale(1.2);margin-left: 20px;" class="icon-plus"></i></a>
+                    </td>
+                </tr>
+            </table>
+            <div style="margin-top:20px;height:10px;border-bottom:1px solid darkgray"></div>
+        </div>
 
-
-                </td>
-            </tr>
-        </table>
         <div>
-            <br>
-            <button class="btn btn-success"><i class="icon-plus icon-white"></i> 增加题目</button>
-            <button class="btn btn-danger pull-right"><i class="icon-minus icon-white"></i> 删除此题</button>
+            <br><br>
+            <button class="btn btn-success add_exercise"><i class="icon-plus icon-white"></i> 增加题目</button>
         </div>
     </div>
-
+    <div>
+        <button class="btn btn-primary btn-large pull-right">确认发布</button>
+    </div>
+    <br><br><br>
 </div>
 </body>
 </html>
